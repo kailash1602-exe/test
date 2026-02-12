6a0a767f9af59214a1a6e4eae396edea7cf8dd8d
kailash1602-exe <kailash@tejaki.com>
2026-02-12 15:36:22 +0530

    Update page.tsx

--- test-my-app/app/page.tsx
+++ test-my-app/app/page.tsx
@@ -2,64 +2,72 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+    <h1>Home Page</h1>
+    </>
   );
 }
+
+// routes
+/*
+1. components
+    1. account
+    2. advertiser
+    3. advertiser-campaign-exec
+    4. advertiser-compliance
+    5. advertiser- creative
+    6. advertiser-finance
+    7. advertiser-marketing
+    8. advertiser-marketing-manager
+    9. advertiser-performance
+    10. auth
+    11. campaign-executive
+    12. cards
+    13. compliance
+    14. creative
+    15. figma
+    16. finance
+    17. marketing
+    18. marketing-manager
+    19. modals
+    20. performance-analyst
+    21. tables
+    22. ui
+    23. billingCompliance.tsx
+    24. Budget.tsx
+    25. Campaign.tsx
+    26. Header.tsx
+    27. Navigation.tsx
+    28. NotificationsPanel.tsx
+    29. Overview.tsx
+    30. Publishers.tsx
+    31. QuickActionBar.tsx
+    32.QuickStarsRow.tsx
+    33. RoleAccessHub.tsx
+    34. Team.tsx
+
+2. data
+    account, finance, creative, compliance, performance, marketing
+
+
+CONCLUSION: there are two files
+1. components
+2. data
+
+i need to create a new folder named structure so that routes can be managed properly.
+
+Currently, due to if else condition, the code is not scalable and maintainable. Also,
+code is working on if-else conditon on one single route localhost:3000 only.
+
+What to do?
+need to create folder based routes, such that a user can access dashboard according to their 
+roles,
+
+
+first there is the page where all the 18 roles are visible and after clicking on any role, 
+user should be redirected to the dashboard of that role and their pages have already been 
+created.
+
+localhost:3000/roles/account-owner {it should be like that but professional}
+
+*/
